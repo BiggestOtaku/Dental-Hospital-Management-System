b213a2ae6278ae0c35296d9c9fada370cedbf125
lakshyasinghal25 <lakshya.singhal.cse23@itbhu.ac.in>
2025-09-14 23:12:31 +0530

    initial commit

---
+++ src/main/java/com/dbms/grp2/DhmsApplication.java
@@ -0,0 +1,13 @@
+package com.dbms.grp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbms/grp2/DhmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbms.grp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
