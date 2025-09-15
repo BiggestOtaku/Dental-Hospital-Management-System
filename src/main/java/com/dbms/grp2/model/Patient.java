95a1d441572dcb1d33963747d4bbced9a06b7700
SilentCodr <vaasukansal40@gmail.com>
2025-09-15 19:32:42 +0530

    sammple entity

---
+++ src/main/java/com/dbms/grp2/model/Patient.java
@@ -0,0 +1,37 @@
+package com.dbms.grp2.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Entity
+@Table(name = "patients")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@ToString
+
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private int age;
+    private String gender;
+    private String address;
+}
