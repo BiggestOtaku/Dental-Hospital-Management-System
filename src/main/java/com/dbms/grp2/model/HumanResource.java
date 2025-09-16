374e3df518ba3100969d1fb12d0fc6c032341ee7
lakshyasinghal25 <lakshya.singhal.cse23@itbhu.ac.in>
2025-09-16 23:55:05 +0530

    HumanResource added

---
+++ src/main/java/com/dbms/grp2/model/HumanResource.java
@@ -0,0 +1,24 @@
+package com.dbms.grp2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="human_resource")
+public class HumanResource {
+    @Id
+    private String hrType;
+
+    @Column(name="capacity", nullable=false)
+    private Integer capacity;
+}
