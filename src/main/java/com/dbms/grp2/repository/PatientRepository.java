a170f7b0dccb3651b04182bd1811253e03ff9bfa
lakshyasinghal25 <lakshya.singhal.cse23@itbhu.ac.in>
2025-09-23 22:21:20 +0530

    one service

--- src/main/java/com/dbms/grp2/repository/PatientRepository.java
+++ src/main/java/com/dbms/grp2/repository/PatientRepository.java
@@ -3,6 +3,9 @@ package com.dbms.grp2.repository;
 import com.dbms.grp2.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientRepository extends JpaRepository<Patient,Long> {
+import java.util.Optional;
 
+public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Optional<Patient> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/dbms/grp2/service/impl/patientServiceImpl.java
+++ src/main/java/com/dbms/grp2/service/impl/patientServiceImpl.java
@@ -2,17 +2,29 @@ package com.dbms.grp2.service.impl;
 
 import com.dbms.grp2.dto.PatientCreateDto;
 import com.dbms.grp2.dto.PatientResponseDto;
+import com.dbms.grp2.model.Patient;
 import com.dbms.grp2.repository.PatientRepository;
 import com.dbms.grp2.service.patientService;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import static com.dbms.grp2.mapper.PatientMapper.toEntity;
+import static com.dbms.grp2.mapper.PatientMapper.toPatientResponse;
 
 @Service
 public class patientServiceImpl implements patientService {
     private PatientRepository patientRepository;
 
     @Override
+    @Transactional
     public PatientResponseDto createPatient(PatientCreateDto dto){
-        PatientResponseDto resDto = null;
-        return resDto;
+        if (patientRepository.existsByEmail(dto.getEmail())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already taken");
+        }
+        Patient patient = toEntity(dto);
+        Patient saved = patientRepository.save(patient);
+        return toPatientResponse(saved);
     }
-}g
+}
